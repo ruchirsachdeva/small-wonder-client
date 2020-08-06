d3403c070f025695f68d0133605c151228472639
ruchir_sachdeva <ruchirsachdeva@gmail.com>
2020-08-06 02:01:45 +0200

    downgraded bootstrap to 5 for angular 8

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express'), http = require('http'), path = require('path');
 const app = express();
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/small-wonder'));
 
 app.listen(process.env.PORT||4200);
 
@@ -10,7 +10,7 @@ app.listen(process.env.PORT||4200);
 // PathLocationStrategy
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'/dist/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/small-wonder/index.html'));
 });
 
 
