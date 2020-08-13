93c7197111df78c9372d72295d4f25cda9ab2769
rishabh <rishabhjairath@gmail.com>
2020-08-14 00:23:45 +0530

    subactivity component

--- src/app/pages/activities-page/activity-display/activity-display.component.ts
+++ src/app/pages/activities-page/activity-display/activity-display.component.ts
@@ -7,6 +7,19 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class ActivityDisplayComponent implements OnInit {
   activityName: string = "";
+  activityImageList = [
+    "/assets/img/kids_race.jpg",
+    "/assets/img/kids_stage.jpg",
+    "/assets/img/kids_yoga.jpg",
+  ];
+  slideConfig = {
+    slidesToShow: 1,
+    nextArrow: "<div class='nav-btn next-slide'></div>",
+    prevArrow: "<div class='nav-btn prev-slide'></div>",
+    dots: true,
+    infinite: true,
+    centerMode: true,
+  };
 
   constructor(private route: ActivatedRoute) {}
 
