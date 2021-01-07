159f2b9cb9b72f948efa993cc4ea27b22d927498
DwarakanathAkkala <37907049+DwarakanathAkkala@users.noreply.github.com>
2021-01-07 17:26:39 +0530

    Changed Background Image text Component css

--- src/app/components/background-img-text/background-img-text.component.ts
+++ src/app/components/background-img-text/background-img-text.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class BackgroundImgTextComponent implements OnInit {
   @Input() title: string = "";
-  @Input() bgImage = "/assets/img/school-building.jpg";
+  @Input() bgImage : string="";
 
   constructor() {}
 
