d3edf90021765f8b2ad1c631c92eb9ab99fa78ec
DwarakanathAkkala <37907049+DwarakanathAkkala@users.noreply.github.com>
2021-01-12 20:57:03 +0530

    Admissions Page Background Image Changed

--- src/app/pages/admissions-page/admissions-page.component.ts
+++ src/app/pages/admissions-page/admissions-page.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AdmissionsPageComponent implements OnInit {
   title: string = "Admission";
+  bgImage: string = "/assets/img/school-building.jpg";
   introText: string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
   sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`;
