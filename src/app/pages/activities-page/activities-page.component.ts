65ec77967f3e25a9c1af42d8366e84dd373c08d3
DwarakanathAkkala <37907049+DwarakanathAkkala@users.noreply.github.com>
2021-01-12 20:56:04 +0530

    Activities Page - Added Explore Section Component Changed Background Image

--- src/app/pages/activities-page/activities-page.component.ts
+++ src/app/pages/activities-page/activities-page.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ActivitiesPageComponent implements OnInit {
   title: string = "Activities";
+  bgImage: string ="/assets/img/activities_cover.jpg";
 
   testimonialList = [
     {
