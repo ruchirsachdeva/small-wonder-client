a49f7d47b8f131dbc661539cc34196d4427c85df
DwarakanathAkkala <37907049+DwarakanathAkkala@users.noreply.github.com>
2021-02-04 22:36:54 +0530

    Home Page Franchise section routes added

--- src/app/pages/home-page/franchise-opportunity/franchise-opportunity.component.ts
+++ src/app/pages/home-page/franchise-opportunity/franchise-opportunity.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-franchise-opportunity',
   templateUrl: './franchise-opportunity.component.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FranchiseOpportunityComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToFranchise(){
+    this.router.navigateByUrl('/franchise');
   }
 
 }
