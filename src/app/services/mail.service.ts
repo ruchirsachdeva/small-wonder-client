479d5722fa3e32c9d710a0228b2d40b445c1fe9a
ruchir_sachdeva <ruchirsachdeva@gmail.com>
2021-10-14 01:19:48 +0200

    Added email support

---
+++ src/app/services/mail.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import emailjs from 'emailjs-com';
+
+
+const templateParams = {
+  from_name: 'Ruchir',
+  to_name: 'Enquiry team',
+  message: 'Check this out!',
+  reply_to: 'ruchirsachdeva@yahoo.com'
+};
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MailService {
+
+  constructor() {
+    emailjs.init('user_j1poPFQiQL7NeK76EVAuH');
+  }
+
+  send() {
+    emailjs.send('ruchir-gmail', 'template_y5dkc0b', templateParams)
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      }, (err) => {
+        console.log('FAILED...', err);
+      });
+  }
+}
