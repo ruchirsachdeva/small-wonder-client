0faaf512473d428c84a3e093025e02e84f10b044
Urvish <urvish@digibee.co>
2023-03-06 19:20:42 +0530

    Histry, Philosophy section

--- src/app/pages/home-page/home-intro-section/home-intro-section.component.ts
+++ src/app/pages/home-page/home-intro-section/home-intro-section.component.ts
@@ -7,9 +7,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeIntroSectionComponent implements OnInit {
 
+  // $('.slider-for').slick({
+  slideConfig_for = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  }
+  // });
+  //   $('.slider-nav').slick({
+  slideConfig_nav = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // nextArrow: "<div class='nav-btn next-slide'></div>",
+    // prevArrow: "<div class='nav-btn prev-slide'></div>",
+    asNavFor: '.slider-for'
+    
+    // centerMode: true,
+    // focusOnSelect: true
+  };
+
+  slideConfig = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: "<div class='nav-btn next-slide'></div>",
+    prevArrow: "<div class='nav-btn prev-slide'></div>",
+    // centerMode: true,
+  };
+
+  branchList = [
+    {
+      imgSrc: "/assets/img/philosophy.png",
+      title: "Our Philosophy"
+    },
+    {
+      imgSrc: "/assets/img/team.png",
+      title: "Meet Our Team"
+    },
+    {
+      imgSrc: "/assets/img/admission.png",
+      title: "Admissions"
+    },
+    {
+      imgSrc: "/assets/img/philosophy.png",
+      title: "Our Philosophy1"
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  afterChange(e) {
+    // debugger;
+  }
 }
